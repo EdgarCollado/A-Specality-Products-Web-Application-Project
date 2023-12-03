60bae29a57f9ab3e8da747b33b732e319201c714
EdgarCollado <edgarcollado@pursuit.org>
2023-12-02 22:21:42 -0500

    project-updated

--- index.js
+++ index.js
@@ -1,89 +1,154 @@
-let shoppingIcon = document.querySelector('.shoppingCart');
+let iconCart = document.querySelector('.shoppingCart');
 let cart = document.querySelector('.cart');
 let container = document.querySelector('.container');
 let close = document.querySelector('.close');
 
-shoppingIcon.addEventListener('click', () => {
-    if (cart.style.right == '-100%') {
+iconCart.addEventListener('click', function(){
+    if(cart.style.right == '-100%'){
         cart.style.right = '0';
         container.style.transform = 'translateX(-400px)';
-    } else {
+    }else{
         cart.style.right = '-100%';
         container.style.transform = 'translateX(0)';
     }
 })
-
-close.addEventListener('click', () => {
+close.addEventListener('click', function (){
     cart.style.right = '-100%';
     container.style.transform = 'translateX(0)';
 })
 
-let products = null;
 
+let products = null;
+// get data from file json
 fetch('product.json')
-.then(response => response.json())
-.then(data => {
-    products = data;
-    addDataToHTML();
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
 })
 
-function addDataToHTML() {
-    let productsListHTML = document.querySelector('.ourProducts')
-    productsListHTML.innerHTML = '';
+//show datas product in list 
+function addDataToHTML(){
+    // remove datas default from HTML
+    let listProductHTML = document.querySelector('.listProduct');
+    listProductHTML.innerHTML = '';
 
-    if(products != null) {
+    // add new datas
+    if(products != null) // if has data
+    {
         products.forEach(product => {
             let newProduct = document.createElement('div');
             newProduct.classList.add('item');
             newProduct.innerHTML = 
-            `<img src="${product.image}"
-        <h2>${product.name}</h2>
-        <div class="price">$${product.price}</div>
-        <button onclick="addCart(${product.id})">Add To Cart</button>`;
-        productsListHTML.appendChild(newProduct);
-        })
+            `<img src="${product.image}" alt="">
+            <h2>${product.name}</h2>
+            <div class="price">$${product.price}</div>
+            <button onclick="addCart(${product.id})">Add To Cart</button>`;
+
+            listProductHTML.appendChild(newProduct);
+
+        });
     }
 }
+//use cookie so the cart doesn't get lost on refresh page
+
 
 let listCart = [];
+function checkCart(){
+    var cookieValue = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('listCart='));
+    if(cookieValue){
+        listCart = JSON.parse(cookieValue.split('=')[1]);
+    }else{
+        listCart = [];
+    }
+}
+checkCart();
+function addCart($idProduct){
+    let productsCopy = JSON.parse(JSON.stringify(products));
+    //// If this product is not in the cart
+    if(!listCart[$idProduct]) 
+    {
+        listCart[$idProduct] = productsCopy.filter(product => product.id == $idProduct)[0];
+        listCart[$idProduct].quantity = 1;
+    }else{
+        //If this product is already in the cart.
+        //I just increased the quantity
+        listCart[$idProduct].quantity++;
+    }
+    document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
 
+    addCartToHTML();
+}
+addCartToHTML();
+function addCartToHTML(){
+    // clear data default
+    let listCartHTML = document.querySelector('.listCart');
+    listCartHTML.innerHTML = '';
 
+    let totalHTML = document.querySelector('.totalQuantity');
+    let totalQuantity = 0;
+    // if has product in Cart
+    if(listCart){
+        listCart.forEach(product => {
+            if(product){
+                let newCart = document.createElement('div');
+                newCart.classList.add('item');
+                newCart.innerHTML = 
+                    `<img src="${product.image}">
+                    <div class="content">
+                        <div class="name">${product.name}</div>
+                        <div class="price">$${product.price} / 1 product</div>
+                    </div>
+                    <div class="quantity">
+                        <button onclick="changeQuantity(${product.id}, '-')">-</button>
+                        <span class="value">${product.quantity}</span>
+                        <button onclick="changeQuantity(${product.id}, '+')">+</button>
+                    </div>`;
+                listCartHTML.appendChild(newCart);
+                totalQuantity = totalQuantity + product.quantity;
+            }
+        })
+    }
+    totalHTML.innerText = totalQuantity;
+}
+function changeQuantity($idProduct, $type){
+    switch ($type) {
+        case '+':
+            listCart[$idProduct].quantity++;
+            break;
+        case '-':
+            listCart[$idProduct].quantity--;
 
+            // if quantity <= 0 then remove product in cart
+            if(listCart[$idProduct].quantity <= 0){
+                delete listCart[$idProduct];
+            }
+            break;
+    
+        default:
+            break;
+    }
+    // save new data in cookie
+    document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
+    // reload html view cart
+    addCartToHTML();
+}
 
+// ... (your existing code)
 
+function emptyCart() {
+    // Clear the listCart array
+    listCart = [];
 
+    // Save the empty cart to the cookie
+    document.cookie = "listCart=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
+    // Reload the HTML view of the cart
+    addCartToHTML();
+}
 
-
-
-
-// document.getElementById("order-form").addEventListener("submit", event => {
-//     event.preventDefault();
-
-//     let itemName = document.getElementById("item-name").value;
-//     let itemQuantity = document.getElementById("item-quantity").value;
-
-
-
-//     event.target.reset();
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const form = document.getElementById("inventory-form");
-//     const inventoryDisplay = document.querySelector(".inventory-display");
-
-//     form.addEventListener("submit", function(event) {
-//         event.preventDefault();
-//         addInventoryItem();
-//     });
-// });
-
-// function addInventoryItem() {
-//     const name = document.getElementById("name").value;
-//     const price = document.getElementById("price").value;
-//     const inventoryNumber = document.getElementById("inventoryNumber").value;
-//     const inStock = document.getElementById("inStock").checked;
-
-//     const itemDiv = document.createElement("div");
-//     itemDiv.classList.add("inventory-item");
-// }
+// Add an event listener to the "Empty Cart" button
+let emptyCartButton = document.querySelector('.emptyCart');
+emptyCartButton.addEventListener('click', emptyCart);
